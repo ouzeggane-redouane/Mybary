37f2f1023f11e352d5a1ab7c6cc8cc0fc8c3acfa
= <rouzeggane@gmail.com>
2024-08-02 07:50:29 +0100

    Initial Authro

---
+++ routes/author.route.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = express.Router();
+
+const Author = require('../models/author.model');
+
+router.get('/', async (req, res) => {
+    let searchOptions = {};
+    let name = '';
+    if (req.query.name != null && req.query.name != ''){
+        searchOptions.name = new RegExp( req.query.name, 'i' );
+    }
+
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render( 'authors/index' , {
+            authors, 
+            searchOptions:name
+        });
+    }
+    catch(err){
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+router.get('/new', (req, res) => {
+    res.render( 'authors/new' , {author: new Author()} );
+})
+
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try{
+        const newAuthor = await author.save();
+
+        // res.redirect( `authors/${newAuthor.id}` );
+        res.redirect( `authors` );
+    }
+    catch(err){
+        res.render('authors/new', {author, errorMessage:'Error creating Author !!!'})
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,7 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 const mongoose = require('mongoose')
-
-const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -16,12 +15,18 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayout)
 app.use(express.static( __dirname + '/public'))
 
+app.use(bodyParser.urlencoded({limit : '10mb', extended:false}));
+
 mongoose.connect(process.env.DB_URL, {useNewUrlParser:true})
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', ()=>console.log('Connected to Mongo-DB'))
 
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author.route')
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 const PORT = process.env.PORT || 3050;
 app.listen(PORT, ()=>{
